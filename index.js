d3c7c65e8ba478cbf6fd2d86d1581e8756daefeb
dazzel3 <aakk9350@naver.com>
2021-09-11 15:04:12 +0900

    mongoDB Connect

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const mongoose = require('mongoose')
+mongoose
+.connect('mongodb+srv://dazzel3:dazzle301@react-node-cluster.w6ih3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+.then(() => console.log('mongoDB Connected'))
+.catch(err => console.log(err))
+
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
+
+
